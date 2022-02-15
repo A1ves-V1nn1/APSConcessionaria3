0de48adfc6aa38860c654e1d5fa6193bc799468c
A1ves-V1nn1 <94202950+A1ves-V1nn1@users.noreply.github.com>
2022-02-15 13:17:33 -0300

    Update ProjetoAPS.java

--- ProjetoAPS.java
+++ ProjetoAPS.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProjetoAPS {
+    
     public static ArrayList<Cliente> lista = new ArrayList();
+    public static ArrayList<veiculos> lista1 = new ArrayList();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Escolha uma das opções:\n1) Cadastrar Cliente\n2) Apagar Cliente\n3) Mostrar clientes\n4) Sair");
+            System.out.println("Escolha uma das opções:\n1) Cadastrar Cliente\n2) Apagar Cliente\n3) Mostrar clientes\n4)Logar como funcionário\n5) Sair");
             int op = scanner.nextInt();
             switch (op) {
                 case 1:
@@ -23,6 +25,9 @@ public class ProjetoAPS {
                     MostrarClientes();
                     break;
                 case 4:
+                    Logarfuncionario();
+                    return;
+                case 5:
                     return;
                 default:
                     System.out.println("Erro ao escolher a opção");
@@ -57,5 +62,61 @@ lista.remove(numero);
         }
         System.out.println("\n\n\n");
     }
+    public static void Logarfuncionario(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite seu código de funcionario: ");
+        int codigo = scanner.nextInt();
+        if (codigo == 12334){
+            System.out.println("Bem vindo Vinícius!");
+            while (true) {
+                System.out.println("Escolha uma das opções:\n1) Cadastrar Veiculo\n2) Apagar Veiculo\n3) Mostrar Veiculos\n4)Sair");
+                int op = scanner.nextInt();
+                switch (op) {
+                case 1:
+                    CadastrarVeiculo();
+                    break;
+                case 2:
+                    System.out.println("Digite a posição do veiculo para apagar");
+                    int numero = scanner.nextInt();
+                    ApagarVeiculo(numero);
+                    break;
+                case 3:
+                    MostrarVeiculo();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Erro ao escolher a opção");
+            }
+        public static void CadastrarVeiculo(){
+            Scanner scanne = new Scanner(System.in);
+            System.out.println("Digite o valor do veiculo:");
+            double valor=scanne.nextDouble();
+            System.out.println("Digite o ano do veiculo");
+            int ano = scanne.nextInt();
+            System.out.println("Digite o modelo do veiculo");
+            String modelo = scanne.next();
+            System.out.println("Digite a marca do veiculo");
+            String marca = scanne.next();
+            veiculos veiculo = new veiculos(valor,ano,modelo,marca);
+            lista1.add(veiculo);
+            }
+            
+            public static void ApagarVeiculo(int numero){
+            lista.remove(numero);
+            }
+            public static void MostrarVeiculo(){
+                System.out.println("=== Cadastrados agora ===");
+                int cont =0;
+                System.out.println("\n\n\n");
+                for (veiculos veiculo: lista1) {
+                    System.out.println(cont + " | " + veiculo);
+                    cont++;
+                }
+                System.out.println("\n\n\n");
+                System.out.println("=== Lista de veiculos disponiveis === \n Modelo= Ford KA \n Valor= 53.000,00 \n Marca= Ford");
+            }
+        }
+    }
 }
-   
+}
