c08fcda2ddfe6fc4ab8c2d7b773351d4bdffc720
A1ves-V1nn1 <94202950+A1ves-V1nn1@users.noreply.github.com>
2022-03-15 19:42:40 -0300

    Update Veiculos.java

--- Veiculos.java
+++ Veiculos.java
@@ -1,57 +1,66 @@
-package projetoaps;
+package model;
 
-import java.util.Scanner;
-import static projetoaps.ProjetoAPS.lista1;
 
-public class veiculos extends ProjetoAPS{
-    private double Valor;
-    private int Ano;
-    private String Modelo;
-    private String Marca;
+public class Veiculo {
+    private int codigo;
+    private String marca;
+    private String modelo;
+    private String ano;
+    private String valor;
 
-    public veiculos() {
-
+    public Veiculo() {
     }
 
-    public veiculos(double Valor,int Ano,String Modelo,String Marca) {
-        this.Valor = Valor;
-        this.Ano = Ano;
-        this.Modelo = Modelo;
-        this.Marca = Marca;
+    public Veiculo(int codigo, String marca, String modelo, String ano, String valor) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.valor = valor;
     }
 
-    public double getValor() {
-        return Valor;
+
+    public int getCodigo() {
+        return codigo;
     }
 
-    public void setValor(double Valor) {
-        this.Valor = Valor;
-    }
-    public int getAno() {
-        return Ano;
-    }
-
-    public void setAno(int Ano) {
-        this.Ano = Ano;
-    }
-
-    public String getModelo() {
-        return Modelo;
-    }
-
-    public void setModelo(String Modelo) {
-        this.Modelo = Modelo;
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
     public String getMarca() {
-        return Marca;
+        return marca;
     }
 
-    public void setMarca(String Marca) {
-        this.Marca = Marca;
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
-   @Override
-    public String toString(){
-        return getModelo();
+
+    public String getModelo() {
+        return modelo;
     }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+    
 }
+
